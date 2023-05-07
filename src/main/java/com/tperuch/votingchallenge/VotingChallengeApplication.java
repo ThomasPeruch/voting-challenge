908fc374d7aee9876881426968443d7b50998f83
ThomasPeruch <peruchthomas@gmail.com>
2023-05-07 18:56:53 -0300

    Initial commit

---
+++ src/main/java/com/tperuch/votingchallenge/VotingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.tperuch.votingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tperuch/votingchallenge/VotingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tperuch.votingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
